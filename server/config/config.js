9a8800ded2c775975bc0a01998f019fbc20c35e9
Matias <matiasefontoura@gmail.com>
2019-03-23 18:05:11 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -5,3 +5,19 @@ PUERTO
 
 
 process.env.PORT = process.env.PORT || 3000;
+
+/* Entorno */
+process.env.Node_ENV = process.env.NODE_Env || 'dev';
+
+/* Base de datos */
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:<4570ampcwc!>@cluster0-kslew.mongodb.net/cafe'
+
+}
+
+process.env.URLDB = urlDB;
